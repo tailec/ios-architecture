23390e557c12d49b95c2e0f8c92eccc33560e598
Dmytro Shulzhenko <dmytro.shulzhenko@gen.tech>
2022-01-24 10:45:35 +0200

    cleanup

--- tmdb-rx-driver/tmdb-rx-driver/Utils/RxUtils/Latest.swift
+++ tmdb-rx-driver/tmdb-rx-driver/Utils/RxUtils/Latest.swift
@@ -15,26 +15,4 @@ extension ObservableType {
         subscribe(onNext: { element = $0 }).dispose()
         return element
     }
-    
-    var await: Element {
-        return latestIfReplayed ?? await(for: { })
-    }
-    
-    func await(for trigger: () -> Void) -> Element {
-        let group = DispatchGroup()
-        
-        var value: Element!
-        var disposable: Disposable!
-        
-        group.enter()
-        disposable = subscribe(onNext: { newValue in
-            value = newValue
-            disposable.dispose()
-            group.leave()
-        })
-        trigger()
-        group.wait()
-        
-        return value
-    }
 }
